53804b7e2c622ad610131ee18e096cab1df8a010
kura123 <b1616730@planet.kanazawa-it.ac.jp>
2020-06-16 19:04:22 +0900

    20200616ここまで

---
+++ chapter3/20.py
@@ -0,0 +1,8 @@
+# 20. JSONデータの読み込みPermalink
+# Wikipedia記事のJSONファイルを読み込み，「イギリス」に関する記事本文を表示せよ．
+# 問題21-29では，ここで抽出した記事本文に対して実行せよ．
+import pandas as pd
+
+df = pd.read_json('jawiki-country.json.gz', lines=True)
+text = df. query('title=="イギリス"')['text'].values[0]
+print(text)
